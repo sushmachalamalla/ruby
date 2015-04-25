61e0dae4f741895128831b9c51a21b1fdd611774
sushmachalamalla <sushma92.ch@gmail.com>
2015-04-24 21:06:47 -0500

    Initial Commit

---
+++ onpurples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onpurples
+//
+//  Created by Sushma Reddy on 4/24/15.
+//  Copyright (c) 2015 Sushma Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
